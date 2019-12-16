784361545ae897d88205ab4f738c1d464babc509
grewa1 <gbjrmail@gmail.com>
2019-12-16 14:45:31 -0500

    navbar responsive. hamburger menue. building with bootstrap

---
+++ portfolio-gregoire/src/App.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+import "./App.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Gregoire Boisrond Jr",
+      headerLinks: [
+        { title: "Home", path: "/" },
+        { title: "About", path: "/about" },
+        { title: "Contact", path: "/contact" }
+      ],
+      home: {
+        title: "Software Engineer",
+        subTitle: "The art of science",
+        smText: "Checkout my projects below"
+      },
+      about: {
+        title: "About Me"
+      },
+      contact: {
+        title: "Contact Me"
+      }
+    };
+  }
+
+  render() {
+    return (
+      <Router>
+        <Container className="p-0" fluid={true}>
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Gregoire Boisrond Jr</Navbar.Brand>
+          <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+          <Navbar.Collapse id="navbar-toggle">
+            <Nav className="ml-auto">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+              <Link className="nav-link" to="/contact">
+                Contact
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+          </Navbar>
+        </Container>
+      </Router>
+    );
+  }
+}
+
+export default App;
