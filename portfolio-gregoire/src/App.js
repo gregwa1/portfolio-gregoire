71821f1650ac61ee22cd269db94685bc1d414ac8
grewa1 <gbjrmail@gmail.com>
2019-12-16 21:58:48 -0500

    footer initialized via bootstrap

--- portfolio-gregoire/src/App.js
+++ portfolio-gregoire/src/App.js
@@ -3,8 +3,11 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-
 import "./App.css";
+
+import Footer from "./components/Footer";
+
+
 
 class App extends React.Component {
   constructor(props) {
@@ -51,6 +54,10 @@ class App extends React.Component {
             </Nav>
           </Navbar.Collapse>
           </Navbar>
+
+          <Footer />
+
+
         </Container>
       </Router>
     );
