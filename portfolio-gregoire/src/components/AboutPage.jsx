a5f75c2526acdeb5722286641cd70853111c5f64
grewa1 <gbjrmail@gmail.com>
2019-12-19 20:25:07 -0500

    about me is done. need to fill it

--- portfolio-gregoire/src/components/AboutPage.jsx
+++ portfolio-gregoire/src/components/AboutPage.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from "react";
+import Hero from "./Hero";
+import Content from "./Content";
 
 function AboutPage(props) {
   return (
-    <p>AboutPage Works!</p>
+    <div>
+      <Hero title={props.title} />
+      <Content>
+        <p>Salute, my name is Gregoire Boisrond Jr. A full stack engineer.</p>
+      </Content>
+    </div>
   );
-
-  } 
+}
 
 export default AboutPage;
