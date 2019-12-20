c95e5790a4f3b2859d00adee465e698c2321a18e
grewa1 <gbjrmail@gmail.com>
2019-12-19 21:28:06 -0500

    form is finished

--- portfolio-gregoire/src/components/ContactPage.jsx
+++ portfolio-gregoire/src/components/ContactPage.jsx
@@ -1,10 +1,60 @@
-import React from 'react'
+import React from 'react';
+import Hero from './Hero';
+import Content from './Content';
+import Form from 'react-bootstrap/Form';
 
-function ContactPage(props) {
-  return (
-    <p>ContactPage Works!</p>
-  );
+class ContactPage extends React.Component {
 
-  } 
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      message: "",
+      disabled: false,
+      emailSent: null
+  
+
+    }
+  }
+
+  render() {
+
+    return (
+      <div>
+        <Hero title={this.props.title} />
+
+        <Content>
+          <Form>
+            <Form.Group>
+              <Form.Label htmlFor="full-name">
+                Full Name
+              </Form.Label>
+              <Form.Control id="full-name" name="name" type="text" value={this.state.name} onChange={this.handleChange} />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label htmlFor="email">
+                Email
+              </Form.Label>
+              <Form.Control id="email" name="email" type="email" value={this.state.email} onChange={this.handleChange} />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label htmlFor="message">
+                Message
+              </Form.Label>
+              <Form.Control id="message" name="message" as="textarea" value={this.state.message} onChange={this.handleChange} />
+            </Form.Group>
+
+          </Form>
+        </Content>
+
+
+      </div>
+    );
+
+  }
+}
 
 export default ContactPage;
