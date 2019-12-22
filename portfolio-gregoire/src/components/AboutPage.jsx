06e0ffd622e747d9fc4ac684d06adeba79fc19b0
grewa1 <gbjrmail@gmail.com>
2019-12-22 13:44:55 -0500

    about me done. need logos in footer

--- portfolio-gregoire/src/components/AboutPage.jsx
+++ portfolio-gregoire/src/components/AboutPage.jsx
@@ -7,7 +7,12 @@ function AboutPage(props) {
     <div>
       <Hero title={props.title} />
       <Content>
-        <p>Salute, my name is Gregoire Boisrond Jr. A full stack engineer.</p>
+        <p>Salute, My name is Gregoire Boisrond Jr. I want to invite you to get to know a little about me. </p>
+          
+        <p>I am software engineer, a lover of science, and having a keen eye on for aesthetics allows me ease of use and 
+           an understanding for web-design and app development.</p>
+
+        <p>My evolution from civil engineering, tech-retail, and computer science was great journey of science. The tie that binds is finding is an algorithm of balance. I can explain further in a one to one. Hear from you soon.</p>
       </Content>
     </div>
   );
