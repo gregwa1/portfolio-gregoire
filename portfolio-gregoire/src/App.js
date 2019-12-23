9e7ec6e97970f6d4fd5dd534af546f3014cc75d1
grewa1 <gbjrmail@gmail.com>
2019-12-22 22:38:43 -0500

    resume link added

--- portfolio-gregoire/src/App.js
+++ portfolio-gregoire/src/App.js
@@ -6,11 +6,9 @@ import Nav from "react-bootstrap/Nav";
 import "./App.css";
 
 import Footer from "./components/Footer";
-import HomePage from './components/HomePage';
-import AboutPage from './components/AboutPage';
-import ContactForm from './components/ContactForm';
-
-
+import HomePage from "./components/HomePage";
+import AboutPage from "./components/AboutPage";
+import ContactForm from "./components/ContactForm";
 
 class App extends React.Component {
   constructor(props) {
@@ -20,7 +18,8 @@ class App extends React.Component {
       headerLinks: [
         { title: "Home", path: "/" },
         { title: "About", path: "/about" },
-        { title: "Contact", path: "/contact" }
+        { title: "Contact", path: "/contact" },
+        { title: "Resume", path: "https://www.docdroid.net/McSZWTM/boisrond-greg-2019-resume.pdf" },
       ],
       home: {
         title: "Software Engineer",
@@ -32,7 +31,11 @@ class App extends React.Component {
       },
       contact: {
         title: "Contact Me"
+      },
+      resume: {
+        title: "Resume"
       }
+
     };
   }
 
@@ -40,33 +43,58 @@ class App extends React.Component {
     return (
       <Router>
         <Container className="p-0" fluid={true}>
-          <Navbar className="border-bottom" text="text-light" bg="black" expand="lg">
-            <Navbar.Brand className="text-light">Gregoire Boisrond Jr</Navbar.Brand>
-          <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
-          <Navbar.Collapse id="navbar-toggle">
-            <Nav className="ml-auto">
-              <Link className="nav-link text-light" to="/">
-                Home
-              </Link>
-              <Link className="nav-link text-light" to="/about">
-                About
-              </Link>
-              <Link className="nav-link text-light" to="/contact">
-                Contact
-              </Link>
-                
-            </Nav>
-          </Navbar.Collapse>
+          <Navbar
+            className="border-bottom"
+            text="text-light"
+            bg="black"
+            expand="lg"
+          >
+            <Navbar.Brand className="text-light">
+              Gregoire Boisrond Jr
+            </Navbar.Brand>
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link text-light" to="/">
+                  Home
+                </Link>
+                <Link className="nav-link text-light" to="/about">
+                  About
+                </Link>
+                <Link className="nav-link text-light" to="/contact">
+                  Contact
+                </Link>
+                <a href="https://www.docdroid.net/McSZWTM/boisrond-greg-2019-resume.pdf"
+                  className="nav-link text-light"
+                >
+                  Resume
+                </a>
+              </Nav>
+            </Navbar.Collapse>
           </Navbar>
-          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
-          <Route path="/about" exact render={() => <AboutPage title={this.state.about.title} />} />
-          <Route path="/contact" exact render={() => <ContactForm title={this.state.contact.title} />} />
-
-
+          <Route
+            path="/"
+            exact
+            render={() => (
+              <HomePage
+                title={this.state.home.title}
+                subTitle={this.state.home.subTitle}
+                text={this.state.home.text}
+              />
+            )}
+          />
+          <Route
+            path="/about"
+            exact
+            render={() => <AboutPage title={this.state.about.title} />}
+          />
+          <Route
+            path="/contact"
+            exact
+            render={() => <ContactForm title={this.state.contact.title} />}
+          />
 
           <Footer />
-
-
         </Container>
       </Router>
     );
