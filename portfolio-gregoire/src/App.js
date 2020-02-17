4a60902916d52c656b9248e2d2bd47342610b883
grewa1 <gbjrmail@gmail.com>
2020-02-16 19:46:30 -0500

    revised background

--- portfolio-gregoire/src/App.js
+++ portfolio-gregoire/src/App.js
@@ -19,7 +19,7 @@ class App extends React.Component {
         { title: "Home", path: "/" },
         { title: "About", path: "/about" },
         { title: "Contact", path: "/contact" },
-        { title: "Resume", path: "https://www.docdroid.net/McSZWTM/boisrond-greg-2019-resume.pdf" },
+        { title: "Resume", path: "https://www.docdroid.net/McSZWTM/boisrond-greg-2019-resume.pdf"},
       ],
       home: {
         title: "Software Engineer",
